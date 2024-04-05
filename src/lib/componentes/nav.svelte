ff1cbd28dcbaf22ec7bd843a7f24a8bfb80e71cb
Ana Beatriz <anabferreira0203@gmail.com>
2024-04-05 11:45:36 -0300

    tamanho da logo

--- src/lib/componentes/nav.svelte
+++ src/lib/componentes/nav.svelte
@@ -1,6 +1,8 @@
-<nav class="container mx-auto mt-10 flex flex-row justify-between rounded-xl bg-slate-400 p-10">
+<nav
+	class="container mx-auto mt-10 flex flex-row items-center justify-between rounded-xl bg-slate-400 p-10"
+>
 	<div>
-		<img src="/logo/Logo.webp" alt="" />
+		<img class="h-auto w-44" src="/logo/Logo.webp" alt="" />
 	</div>
 	<div class="flex flex-row space-x-10">
 		<a class=" text-white hover:text-slate-700" href="/">Produtos</a>
