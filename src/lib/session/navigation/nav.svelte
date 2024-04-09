2cec2f8753207ff91a46ee162ad0ac1072de1761
Ana Beatriz <anabferreira0203@gmail.com>
2024-04-09 13:32:23 -0300

    style da nav

--- src/lib/session/navigation/nav.svelte
+++ src/lib/session/navigation/nav.svelte
@@ -1,12 +1,23 @@
-<nav
-	class="rounded-x container mx-auto mt-10 flex flex-row items-center justify-between rounded-xl bg-white/10 p-10 drop-shadow-md backdrop-blur-sm"
->
-	<div>
-		<img class="h-auto w-44" src="/logo/Logo.webp" alt="" />
-	</div>
-	<div class=" flex flex-row space-x-10">
-		<a class=" text-white hover:text-slate-700" href="/">Produtos</a>
-		<a class=" text-white hover:text-slate-700" href="/">Informações</a>
-		<a class=" text-white hover:text-slate-700" href="/">Contato</a>
+<script>
+	import { Menu } from 'lucide-svelte';
+</script>
+
+<nav class="section-spacing-padding-x">
+	<div
+		class="nav-bar style-nav container flex-row-reverse justify-between space-x-reverse lg:flex-row"
+	>
+		<div>
+			<img class="h-auto w-44" src="/logo/Logo.webp" alt="" />
+		</div>
+		<div class="hidden flex-row space-x-10 lg:flex">
+			<a class=" text-white hover:text-slate-700" href="/">Produtos</a>
+			<a class=" text-white hover:text-slate-700" href="/">Informações</a>
+			<a class=" text-white hover:text-slate-700" href="/">Contato</a>
+		</div>
+		<div class="flex lg:hidden">
+			<button >
+				<Menu color="#ffff" class="h-10 w-auto"></Menu>
+			</button>
+		</div>
 	</div>
 </nav>
