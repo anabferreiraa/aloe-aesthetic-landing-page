0005c49f1f2147bec37d1c143e623031c254c7e0
Ana Beatriz <anabferreira0203@gmail.com>
2024-04-09 15:07:50 -0300

    criação do modal pra nav

--- src/lib/session/navigation/nav.svelte
+++ src/lib/session/navigation/nav.svelte
@@ -1,5 +1,7 @@
 <script>
 	import { Menu } from 'lucide-svelte';
+	import Modal from '../../componentes/modal/modal.svelte';
+	let modal;
 </script>
 
 <nav class="section-spacing-padding-x">
@@ -10,14 +12,16 @@
 			<img class="h-auto w-44" src="/logo/Logo.webp" alt="" />
 		</div>
 		<div class="hidden flex-row space-x-10 lg:flex">
-			<a class=" text-white hover:text-slate-700" href="/">Produtos</a>
-			<a class=" text-white hover:text-slate-700" href="/">Informações</a>
-			<a class=" text-white hover:text-slate-700" href="/">Contato</a>
+			<a class=" text-white hover:text-white/80" href="/">Produtos</a>
+			<a class=" text-white hover:text-white/80" href="/">Informações</a>
+			<a class=" text-white hover:text-white/80" href="/">Contato</a>
 		</div>
 		<div class="flex lg:hidden">
-			<button >
+			<button on:click={() => modal.show()}>
 				<Menu color="#ffff" class="h-10 w-auto"></Menu>
 			</button>
 		</div>
 	</div>
 </nav>
+
+<Modal bind:this={modal}></Modal>
