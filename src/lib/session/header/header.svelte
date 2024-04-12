901b7b592ce8040ef1ef7cc2c1e5bd4c0a94bdd2
Ana Beatriz <anabferreira0203@gmail.com>
2024-04-12 14:44:56 -0300

    adicionando fonte

--- src/lib/session/header/header.svelte
+++ src/lib/session/header/header.svelte
@@ -3,18 +3,19 @@
 </script>
 
 <header class="section-spacing-padding-x">
-	<div class="container mx-auto mt-20">
-		<div class="space-y-4">
-			<div class="">
-				<span class="text-white/50">Conheça a</span>
-				<h1 class="text-white">Aloe aesthetic</h1>
-				<div class="">
+	<div class=" container mx-auto mb-32 mt-32 flex space-y-4">
+		<div class="flex flex-col justify-center space-y-4">
+			<div>
+				<span class=" text-white/50">Conheça a</span>
+				<h1 class=" text-white">Aloe aesthetic</h1>
+				<div class=" h-auto w-[85%] lg:w-[40%]">
 					<p class=" text-white">
 						Uma linha completa e especializada em cuidar da sua saúde e bem estar. Sempre
 						PRESERVANDO o meio ambiente.
 					</p>
 				</div>
 			</div>
+
 			<div>
 				<a class="flex flex-row items-center text-white" href="/">Saiba mais <ArrowRight /> </a>
 			</div>
