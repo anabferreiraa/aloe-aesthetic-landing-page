ab852f9cff978890aab2c76873950c390c95784c
Ana Beatriz <anabferreira0203@gmail.com>
2025-10-08 18:21:25 -0300

    criaçao da header

---
+++ src/lib/Components/Nav/Nav.svelte
@@ -0,0 +1,17 @@
+<script>
+	let links = [
+		{ name: 'Produtos', href: '#home' },
+		{ name: 'Informações', href: '#home' },
+		{ name: 'Contato', href: '#home' }
+	];
+</script>
+
+<nav class="effectglass flex justify-between p-10">
+	<div> <h2>imagem</h2>
+    </div>
+	<ul class="flex gap-6 text-green-pea-50 hover:text-green-pea-100">
+        {#each links as { name, href } }
+            <li> <a href={href}>{name}</a></li>
+        {/each}
+	</ul>
+</nav>
---
+++ src/lib/Header/Header.svelte
@@ -0,0 +1,20 @@
+<script>
+	import Nav from "$lib/Components/Nav/Nav.svelte";
+
+</script>
+<header class="bg-cover bg-no-repeat bg-center ">
+<div class="container bg-green-pea-800">
+  <Nav/>
+  <div class="grid grid-cols-2">
+   <div class="space-y-4 flex flex-col justify-center ">
+    <span class="text-green-pea-50/60">Conheça a</span>
+    <h1 class=" text-7xl text-green-pea-50">Aloe aesthetic</h1>
+    <p class="text-2xl max-w-lg text-green-pea-50/80">Uma linha completa e especializada em cuidar da sua saúde e bem estar. sempre PRESERVANDO o meio ambiente.</p>
+    <a href="p" class=" bg-green-700 rounded-md py-2 px-4 text-green-pea-50" >Saiba mais</a>
+   </div>
+   <div class="flex justify-center items-center">
+    <img class="" src="image/header/background.webp" alt="">
+   </div>
+  </div>
+</div>
+</header>
