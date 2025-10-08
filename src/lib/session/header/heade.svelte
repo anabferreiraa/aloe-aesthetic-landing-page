70da6c0d2d81bd35da91b8877ce5f90766857c1e
Ana Beatriz <anabferreira0203@gmail.com>
2025-10-08 15:49:05 -0300

    recomeço

--- src/lib/session/header/heade.svelte
+++
@@ -1,1 +0,0 @@
-<header class=" bg-black"></header>
--- src/routes/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	import Nav from '../lib/session/navigation/nav.svelte';
-</script>
-
-<header
-	class="relative flex h-[400px] w-full flex-col justify-between bg-lime-800 py-5 md:h-[400px] md:py-8"
->
-	<Nav></Nav>
-</header>
