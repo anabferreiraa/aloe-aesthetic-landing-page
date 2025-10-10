f8f76e82d414bbec48428ad2d2ae640fa1b026c7
Ana Beatriz <anabferreira0203@gmail.com>
2025-10-10 15:25:31 -0300

    section construida

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
-	import Header from "$lib/Header/Header.svelte";
+	import Header from "$lib/Sections/Header/Header.svelte";
+	import Informative from "$lib/Sections/Informative/Informative.svelte";
 
 
 </script>
 
 <Header/>
+<Informative/>
