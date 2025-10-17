a30c3bae51f5761c5eecbf4ba623fe7374a6e1f5
Ana Beatriz <anabferreira0203@gmail.com>
2025-10-17 12:41:48 -0300

    footer finalizada

--- src/lib/Sections/Header/Header.svelte
+++ src/lib/Sections/Header/Header.svelte
@@ -8,7 +8,7 @@
    <div class="mt-30 text-center md:text-start space-y-5 ">
     <span class="text-green-pea-50/60">Conheça a</span>
     <h1 class="text-4xl lg:text-7xl text-green-pea-50">Aloe aesthetic</h1>
-    <p class="md:text-2xl md:max-w-lg text-green-pea-50/80">Uma linha completa e especializada em cuidar da sua saúde e bem estar. sempre PRESERVANDO o meio ambiente.</p>
+    <p class="md:text-2xl md:max-w-lg text-green-pea-50/80">Uma linha especializada em promover sua saúde e bem-estar, com fórmulas que respeitam a natureza e valorizam o equilíbrio entre você e o planeta.</p>
     <a href="p" class="bg-green-pea-400 hover:bg-green-pea-300 px-5 py-4 cursor-pointer text-green-pea-950 font-medium text-2xl" >Saiba mais</a>
    </div>
 </div>
