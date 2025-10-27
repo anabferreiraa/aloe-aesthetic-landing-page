107ec109b7b96a8f2abf2385d368ea2792f7b9fc
Ana Beatriz <anabferreira0203@gmail.com>
2025-10-27 14:13:32 -0300

    mudança de background da header

--- src/lib/Sections/Header/Header.svelte
+++ src/lib/Sections/Header/Header.svelte
@@ -10,13 +10,13 @@
       href:"#produtos"
   }
 </script>
-<header id="header" class="bg-[url(/image/header/background-header-mobile-container.webp)] md:bg-[url(/image/header/background-header-mobile-tablet-container.webp)] lg:bg-[url(/image/header/background-header-desktop-container.webp)] bg-center bg-no-repeat bg-cover">
-<div class="container h-[956px] md:h-[790px] p-6">
+<header id="header" class="bg-[url(/image/header/Background-header-mobile.webp)] lg:bg-[url(/image/header/background-header-desktop.webp)] bg-center bg-no-repeat bg-cover">
+<div class="container h-[980px] md:h-[1050px] lg:h-[800px] p-6">
   <Nav/>
-   <div class="mt-30 text-center md:text-start space-y-8">
-    <span class="text-green-pea-50/60">{text.subTitle}</span>
-    <h1 class="text-4xl lg:text-7xl text-green-pea-50">{text.title}</h1>
-    <p class="md:text-2xl md:max-w-lg text-green-pea-50/80">{text.descrition}</p>
+   <div class="mt-20 lg:mt-30 text-center lg:text-start lg:space-y-8 space-y-7">
+    <span class="text-green-pea-800/70">{text.subTitle}</span>
+    <h1 class="text-4xl  mb-3 lg:text-7xl text-green-pea-900">{text.title}</h1>
+    <p class="md:text-2xl lg:max-w-lg text-green-pea-950/80">{text.descrition}</p>
     <a href={cta.href} class="bg-green-pea-400 hover:bg-green-pea-300 mt-6 px-5 py-4 cursor-pointer text-green-pea-950 font-medium text-2xl" >{cta.title}</a>
    </div>
 </div>
